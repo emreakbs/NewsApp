d55b05ba7cbc8c5cb554c3cc03e36175cb337cd0
Emre Akbaş <emre.akbs@outlook.com>
2021-02-24 20:46:11 +0300

    ibase interface edit

--- ImageApp.Core/Interface/IBaseModel.cs
+++ ImageApp.Core/Interface/IBaseModel.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace ImageApp.Core.Interface
 {
-   public interface IBaseModel
+    public interface IBaseModel
     {
+        int Id { get; set; }
+        /// <summary>
+        /// oluşturulma tarihi
+        /// </summary>
+        DateTime CreateDate { get; set; }
+        /// <summary>
+        /// düzenlenme tarihi
+        /// </summary>
+        DateTime UpdateDate { get; set; }
+        /// <summary>
+        /// oluşturan kullanıcı
+        /// </summary>
+        int CreateUser { get; set; }
+        /// <summary>
+        /// düzenleyen kullanıcı
+        /// </summary>
+        int UpdateUser { get; set; }
+        /// <summary>
+        /// aktiflik durumu
+        /// </summary>
+        bool Status { get; set; }
     }
 }
